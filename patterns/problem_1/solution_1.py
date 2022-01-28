d5cac790fee2ff79824544e2ba1416970f245a7e
prabhakar <prabhakarreddyguntabavi@gmail.com>
2022-01-28 22:27:22 +0530

    Added solution_1 for problem_1 in patterns

--- patterns/problem_1/solution_1.py
+++ patterns/problem_1/solution_1.py
@@ -1,3 +1,16 @@
-def solution1(n):
-    print(n)
+def solution1(n: int):
+    l = n
+    double_of_n = n + n - 1
+    print((str(n) + " ") * double_of_n)
+    c = ""
+    for i in range(1, l):
+        b = ""
+        b += c + " " + str((str(l - (i)) + " ") * (double_of_n - (2))) + str(l)
+        for j in range(1, l):
+            c += str(l - i)
+            print(c)
+            break
 
+
+def sum_two_numbers(a: int, b: int):
+    return a + b
