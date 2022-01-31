524164c10aeebae328520b833f1aca51107697a8
siva.chenna <siva.chenna@ibhubs.co.in>
2022-01-31 20:00:06 +0530

    Add solution_1.py

---
+++ patterns/problem_1/solution_1.py
@@ -0,0 +1,23 @@
+def solution1(n: int):
+    double_of_n = n + n - 1
+    print((str(n) + " ") * double_of_n)
+    first_half = str(n)
+    second_half = ""
+    appending_numbers = []
+    for i in range(1, n):
+        b = ""
+        b += first_half + " " + str((str(n - (i)) + " ") * (n - (i))) + (str(n - (i)) + (" ")) * (
+                    n - (i + 1)) + second_half + str(n)
+        print(b)
+        appending_numbers.append(b)
+        for descending_order in range(1, n):
+            first_half += " " + str(n - i)
+            break
+        for revers_order in range(1, n):
+            second_half = str(n - i) + " " + second_half
+            break
+
+    appending_numbers = appending_numbers[:-1]
+    for i in range(1, len(appending_numbers) + 1):
+        print(appending_numbers[-i])
+    print((str(n) + " ") * double_of_n)
